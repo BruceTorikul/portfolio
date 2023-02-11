04891ca4e0ab068a8584f8204d9d4bc5510c93d4
BruceTorikul <torikulislamtorikul3416@gmail.com>
2023-02-11 16:22:28 +0600

    first commit

---
+++ resources/js/script.js
@@ -0,0 +1,34 @@
+//mixitUp
+var mixer = mixitup(".grid-portfolio-container");
+
+//scrollify
+$(function () {
+  $.scrollify({
+    section: ".scrollify",
+    setHeights: false,
+    updateHash: false,
+  });
+});
+
+//HAMBURGER MENU
+
+function openMenu() {
+  document.getElementById("navbar").style.height = "100vh";
+}
+
+function closeMenu() {
+  document.getElementById("navbar").style.height = "0";
+}
+
+//SMOOTH SCROLL
+$(function () {
+  $(".menu-item a, .scroll-down a").on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: $($.attr(this, "href")).offset().top,
+      },
+      1000
+    );
+    return false;
+  });
+});
